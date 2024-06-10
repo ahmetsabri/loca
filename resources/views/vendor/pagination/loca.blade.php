28eccaad83e6ce596e98443732683301a84a4665
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2024-06-10 15:02:13 +0300

    remove comment

--- resources/views/vendor/pagination/loca.blade.php
+++ resources/views/vendor/pagination/loca.blade.php
@@ -1,7 +1,6 @@
 @if ($paginator->hasPages())
 
 <div class="pagination flex items-center justify-center gap-2.5 flex-wrap mt-17 2xl:mt-15 xl:mt-13 lg:mt-11 md:mt-9">
-    {{-- @if ($paginator->onFirstPage()) --}}
     <a href="@if ($paginator->onFirstPage())  @else {{ $paginator->previousPageUrl() }}  @endif"
         class="flex  duration-300 items-center justify-center w-6.5 aspect-square shrink-0 rounded-1.5 border border-solid border-[#2675FA] text-3.5 font-medium text-tertiary-950 hover:bg-[#2675FA] hover:text-white ">
         <div class="icon icon-chevron-left text-2 h-2 block leading-none"></div>
