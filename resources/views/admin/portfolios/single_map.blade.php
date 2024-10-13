d453447895b8c052dba75b00a8c7b3719279a6d0
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2024-10-13 15:00:56 +0300

    fix map

--- resources/views/admin/portfolios/single_map.blade.php
+++ resources/views/admin/portfolios/single_map.blade.php
@@ -1,26 +1,21 @@
 <div id="map" style="width:100%;height:450px;"></div>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDw9HPxhdYmLJsF_xVXmEs_f2QwfaEoGho"></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
 <script>
-    function initMap () {
-
-                const a = {{ $model?->lat_lon[0]}};
-                const b = {{ $model?->lat_lon[1]}};
-                const initialLocation = { lat: a, lng: b };
-
-
-
-             const map = new google.maps.Map(document.getElementById("map"), {
-                        zoom: 12, // Adjust the zoom level as needed
-                        center: initialLocation,
-                        });
+    const a = {{ $model?->lat_lon[0] }};
+    const b = {{ $model?->lat_lon[1] }};
+    const title = "{{ $model->title }}";
+    const map = L.map('map').setView([a, b], 13);
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
 
-                    let marker = new google.maps.Marker({
-                        position: initialLocation,
-                        map: map
-                        });
-
-            }
-            window.onload = initMap;
+    const marker = L.marker([a, b]).addTo(map);
+    marker.bindPopup(title).openPopup();
 </script>
