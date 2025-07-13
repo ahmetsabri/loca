b314c94024aae642d404a62901db41601672c241
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2025-07-14 01:06:40 +0300

    fix

--- resources/views/admin/forms/index.blade.php
+++ resources/views/admin/forms/index.blade.php
@@ -83,7 +83,9 @@
                                 class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 <td scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white truncate">
-                                    {{ $form->name }}
+                                    <p class="truncate">
+                                        {{ $form->name }}
+                                    </p>
                                 </td>
                                 <td scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
